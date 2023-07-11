6156774e6ece20061c2a794d71721e55c070becf
Ahyeonn <ahyeon971030@gmail.com>
2023-07-10 19:15:54 -0700

    add: convert flag

--- main.go
+++ main.go
@@ -1,36 +1,101 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 
-	"cloud.google.com/go/translate"
-	"golang.org/x/text/language"
-	"google.golang.org/api/option"
+	"golang.org/x/net/html"
 )
 
+func readHtmlFromFile(fileName string) (string, error) {
+
+	bs, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func parse(text string, tag string) (data []string) {
+
+	tkn := html.NewTokenizer(strings.NewReader(text))
+
+	var vals []string
+
+	var isLi bool
+
+	for {
+
+		tt := tkn.Next()
+
+		switch {
+
+		case tt == html.ErrorToken:
+			return vals
+
+		case tt == html.StartTagToken:
+
+			t := tkn.Token()
+			isLi = t.Data == tag
+
+		case tt == html.TextToken:
+
+			t := tkn.Token()
+
+			if isLi {
+				vals = append(vals, t.Data)
+			}
+
+			isLi = false
+		}
+	}
+}
+
+type Page struct {
+	TextFilePath   string
+	TranslatedPage string
+	Title          string
+	Content        string
+}
+
 func main() {
-	trans := flag.String("trans", "", "google translate")
+	convert := flag.String("convert", "", "convert html to preferred language")
 	flag.Parse()
 
-	if *trans != "" {
-		// Set up a context and authenticate with Google Cloud
-		ctx := context.Background()
-		creds := option.WithCredentialsFile("./credential.json")
-		client, err := translate.NewClient(ctx, creds)
+	// call this flag to indicate which html to translate
+	if *convert != "" {
+		fileName := (*convert)
+		text, err := readHtmlFromFile(fileName)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileTitle := parse(text, "h1")[0] // exists only one
+		fileContent := parse(text, "p")[0]
+
+		pageName := string("translate.html")
+		// fmt.Println(title[0], content[0])
+		page := Page{
+			TextFilePath:   "./",
+			TranslatedPage: pageName,
+			Title:          string(fileTitle),
+			Content:        string(fileContent),
+		}
+		t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+		newFile, err := os.Create(page.TranslatedPage)
 		if err != nil {
 			panic(err)
 		}
-		lenArg := os.Args[1][8:]
-		// Translate a string
-		text := lenArg
-		targetLang := language.Make("es")
-		resp, err := client.Translate(ctx, []string{text}, targetLang, nil)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(resp[0].Text)
+		t.Execute(newFile, page)
+		fmt.Print(">> New translated page has been created")
+		// }
 	}
 }
